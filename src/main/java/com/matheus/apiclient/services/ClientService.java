17e8041b0b450b4681b0127b6fc10a41e9754966
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-01-14 23:41:03 -0300

    Fix: Add annotation Transactional

--- src/main/java/com/matheus/apiclient/services/ClientService.java
+++ src/main/java/com/matheus/apiclient/services/ClientService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,6 +17,7 @@ public class ClientService {
     @Autowired
     private ClientRepository repository;
 
+    @Transactional(readOnly = true)
     public Page<ClientDTO> findAllPaged(Pageable page) {
         Page<Client> entity = repository.findAll(page);
         return entity.map(cli -> new ClientDTO(cli));
