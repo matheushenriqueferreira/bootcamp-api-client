727fcd856f968d1709ffa4fd8f5f9b3b7931b549
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-01-14 23:35:38 -0300

    FindAllPaged

---
+++ src/main/java/com/matheus/apiclient/resources/ClientResource.java
@@ -0,0 +1,30 @@
+package com.matheus.apiclient.resources;
+
+import com.matheus.apiclient.dto.ClientDTO;
+import com.matheus.apiclient.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientResource {
+
+    @Autowired
+    private ClientService service;
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "4") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        Page<ClientDTO> cat = service.findAllPaged(pageRequest);
+        return ResponseEntity.ok().body(cat);
+    }
+}
---
+++ src/main/java/com/matheus/apiclient/services/ClientService.java
@@ -0,0 +1,24 @@
+package com.matheus.apiclient.services;
+
+import com.matheus.apiclient.dto.ClientDTO;
+import com.matheus.apiclient.entities.Client;
+import com.matheus.apiclient.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository repository;
+
+    public Page<ClientDTO> findAllPaged(Pageable page) {
+        Page<Client> entity = repository.findAll(page);
+        return entity.map(cli -> new ClientDTO(cli));
+    }
+
+}
